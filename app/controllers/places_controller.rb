0df77c6749398402875db899ca577dc77f67d109
Jade Cribbs <jccjdb@aol.com>
2019-10-21 00:29:31 +0000

    NomsterL27IC

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -10,10 +10,13 @@ class PlacesController < ApplicationController
   end
 
   def create
-    current_user.places.create(place_params)
-    redirect_to root_path
+    @place = current_user.places.create(place_params)
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
   end
-
+end
   def show
     @place = Place.find(params[:id])
   end
@@ -26,20 +29,25 @@ class PlacesController < ApplicationController
   end
 
   def update
-    @place = Place.find(params[:id])
-    if @place.user != current_user
-      return render plain: 'Not Allowed', status: :forbidden
+  @place = Place.find(params[:id])
+
+  if @place.user != current_user
+    return render plain: 'Not Allowed', status: :forbidden
   end
 
-    @place.update_attributes(place_params)
-    redirect_to root_path
-  end
+  @place.update_attributes(place_params)
+  if @place.valid?
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+end
 
   def destroy
     @place = Place.find(params[:id])
     if @place.user != current_user
       return render plain: 'Not Allowed', status: :forbidden
-      
+
     @place.destroy
     redirect_to root_path
   end
