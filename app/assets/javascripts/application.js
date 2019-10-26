d476b266e9a033bfed5846b045db4ef73e74aa1e
Jade Cribbs <jccjdb@aol.com>
2019-10-26 20:02:43 +0000

    NomsterL34IC

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery_ujs
 //= require rails-ujs
 //= require activestorage
-//= require jquery_ujs
 //= require turbolinks
 //= require popper
 //= require tether
