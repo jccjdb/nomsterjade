f5e4c338a3db20ace19ddcc583c257b3d24935df
Jade Cribbs <jccjdb@aol.com>
2019-10-27 20:40:22 +0000

    NomsterL39IC

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,11 +3,13 @@ class PhotosController < ApplicationController
 
   def create
     @place = Place.find(params[:place_id])
+    @place.photos.create(photo_params.merge(user: current_user))
+    redirect_to place_path(@place)
   end
 
   private
 
   def photo_params
-    params.require(:photo).permit(:user, :place)
+    params.require(:photo).permit(:caption, :user)
   end
 end
