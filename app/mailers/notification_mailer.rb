c8e8941788d014ab78c7e84c1f26480c9dfcbc86
Jade Cribbs <jccjdb@aol.com>
2019-11-02 19:19:33 +0000

    L41C

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,7 @@ class NotificationMailer < ApplicationMailer
   default from: "no-reply@nomsterapp.com"
 
   def comment_added
-    mail(to: "jccjdb@aol.com",
+    mail(to: "jccjdb@gmail.com",
       subject: "A comment has been added to your place")
   end
 end
